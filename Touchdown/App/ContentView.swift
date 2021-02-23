0bbf31b5f0b095b4ef8ed35c492812a3e29347f7
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-23 15:10:07 -0800

    create ProductItemView and add to LazyVGrid in ContentView

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -30,6 +30,17 @@ struct ContentView: View {
             
             CategoryGridView()
             
+            TitleView(title: "Helmets")
+            
+            LazyVGrid(columns: gridLayout, spacing: 15, content: {
+              ForEach(products) { product in
+                ProductItemView(product: product)
+              } //: LOOP
+            }) //: GRID
+            .padding(15)
+            
+            TitleView(title: "Brands")
+            
             FooterView()
               .padding(.horizontal, 40)
               .padding()
---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Joshua Basche on 2/23/21.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+  let id: Int
+  let image: String
+}

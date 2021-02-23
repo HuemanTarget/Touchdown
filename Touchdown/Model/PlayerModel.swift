be0874ea7fd2994cfada639b49bda6f0ab4afe3a
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-23 12:28:42 -0800

    create FeaturedItemView and FeaturedItemTabView and add below Header in ContentView

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Joshua Basche on 2/23/21.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+  let id: Int
+  let image: String
+}

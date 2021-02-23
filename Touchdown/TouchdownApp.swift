a2d70c816045f8e5e97ee070e9bc098081cad066
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-23 10:46:34 -0800

    first commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Joshua Basche on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

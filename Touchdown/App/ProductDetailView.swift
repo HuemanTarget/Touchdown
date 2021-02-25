cbcfb2347d28e88447694a023d3324cddfe05457
Joshua Basche <joshuabasche@Joshuas-MacBook-Pro.local>
2021-02-25 10:32:34 -0800

    add description to ProductDetailView with zIndex of 0

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -22,13 +22,32 @@ struct ProductDetailView: View {
         // DETAIL TOP PART
         TopPartDetailView()
           .padding(.horizontal)
+          .zIndex(1)
+        
         // DETAIL BOTTOM PART
-        // RATINGS + SIZES
-        // DESCRIPTION
-        // QUANTITY + FAVORITE
-        // ADD TO CART
-        Spacer()
+        VStack(alignment: .center, spacing: 0, content: {
+          // RATINGS + SIZES
+          
+          // DESCRIPTION
+          ScrollView(.vertical, showsIndicators: false, content: {
+            Text(sampleProduct.description)
+              .font(.system(.body, design: .rounded))
+              .foregroundColor(.gray)
+              .multilineTextAlignment(.leading)
+          }) //: SCROLL
+          
+          // QUANTITY + FAVORITE
+          // ADD TO CART
+          Spacer()
+        }) //: VSTACK
+        .padding(.horizontal)
+        .background(
+          Color.white
+            .clipShape(CustomShape())
+            .padding(.top, -105)
+        )
       }) //: VSTACK
+      .zIndex(0)
       .ignoresSafeArea(.all, edges: .all)
       .background(
         Color(
